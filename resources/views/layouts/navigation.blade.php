1f1b2474c30b2666490e323846a0d8cc1564ce54
achraf <achraf04chke@gmail.com>
2025-02-28 02:28:54 +0000

    Updated sidebar background color to match the template style.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,70 +1,98 @@
-<aside class="aside is-placed-left is-expanded bg-gray-900 text-white h-screen">
-    <div class="aside-tools p-4 border-b border-gray-700">
-        <div class="text-lg font-bold">
-            Admin <b class="text-purple-500">One</b>
-        </div>
+<aside class="aside is-placed-left is-expanded">
+  <div class="aside-tools">
+    <div>
+      Admin <b class="font-black">One</b>
     </div>
-    <div class="menu is-menu-main">
-        <p class="menu-label text-gray-400 px-4">General</p>
-        <ul class="menu-list">
-            <li class="{{ request()->routeIs('dashboard') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('dashboard') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
-                    <span class="menu-item-label ml-2">Dashboard</span>
-                </a>
-            </li>
-        </ul>
-
-        <p class="menu-label text-gray-400 px-4 mt-4">Examples</p>
-        <ul class="menu-list">
-            <li class="{{ request()->routeIs('clients.index') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('clients.index') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-account-circle"></i></span>
-                    <span class="menu-item-label ml-2">Clients</span>
-                </a>
-            </li>
-            <li class="{{ request()->routeIs('products.index') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('products.index') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-table"></i></span>
-                    <span class="menu-item-label ml-2">Products</span>
-                </a>
-            </li>
-            @if(Auth::user()->role !== 'agent')
-            <li class="{{ request()->routeIs('mediaBuyers.index') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('mediaBuyers.index') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-view-list"></i></span>
-                    <span class="menu-item-label ml-2">Media Buyer</span>
-                </a>
-            </li>
-            @endif
-            <li class="{{ request()->routeIs('leads.index') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('leads.index') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-                    <span class="menu-item-label ml-2">Leads</span>
-                </a>
-            </li>
-            @if (Auth::user()->role !== 'agent')
-            <li class="{{ request()->routeIs('agents.index') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('agents.index') }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-account-tie"></i></span>
-                    <span class="menu-item-label ml-2">Agents</span>
-                </a>
-            </li>
-            @endif
-            <li class="{{ request()->routeIs('agents.stats') ? 'bg-gray-800' : '' }}">
-                <a href="{{ route('agents.stats', ['agentId' => Auth::user()->id]) }}" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-chart-line"></i></span>
-                    <span class="menu-item-label ml-2">Agent Stats</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="flex items-center p-3 hover:bg-gray-800">
-                    <span class="icon"><i class="mdi mdi-lock"></i></span>
-                    <span class="menu-item-label ml-2">Login</span>
-                </a>
-            </li>
-        </ul>
-
-        
-    </div>
+  </div>
+  <div class="menu is-menu-main">
+    <p class="menu-label text-white">General</p>
+    <ul class="menu-list">
+      @if (Auth::user()->role !== 'agent')
+        <li class="--set-active-dashboard">
+          <a href="{{ route('dashboard') }}" class="text-white hover:text-gray-200">
+            <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+            <span class="menu-item-label">Dashboard</span>
+          </a>
+        </li>
+      @endif
+    </ul>
+    <p class="menu-label text-white">Navigation</p>
+    <ul class="menu-list">
+      @if (Auth::user()->role !== 'admin')
+      <li>
+        <a href="{{ route('home') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-home"></i></span>
+          <span class="menu-item-label">Home</span>
+        </a>
+      </li>
+      @endif
+      <li>
+        <a href="{{ route('clients.index') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-account-group"></i></span>
+          <span class="menu-item-label">Clients</span>
+        </a>
+      </li>
+      <li>
+        <a href="{{ route('products.index') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-package-variant"></i></span>
+          <span class="menu-item-label">Products</span>
+        </a>
+      </li>
+      @if (Auth::user()->role !== 'agent')
+      <li>
+        <a href="{{ route('mediaBuyers.index') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-shopping"></i></span>
+          <span class="menu-item-label">Media Buyers</span>
+        </a>
+      </li>
+      @endif
+      <li>
+        <a href="{{ route('leads.index') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-chart-line"></i></span>
+          <span class="menu-item-label">Leads</span>
+        </a>
+      </li>
+      @if (Auth::user()->role !== 'agent')
+      <li>
+        <a href="{{ route('agents.index') }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-account-tie"></i></span>
+          <span class="menu-item-label">Agents</span>
+        </a>
+      </li>
+      @endif
+      <li>
+        <a href="{{ route('agents.stats', ['agentId' => Auth::user()->id]) }}" class="text-white hover:text-gray-200">
+          <span class="icon"><i class="mdi mdi-chart-bar"></i></span>
+          <span class="menu-item-label">Agent Stats</span>
+        </a>
+      </li>
+    </ul>
+    <p class="menu-label text-white">User</p>
+    <ul class="menu-list">
+      @if (Auth::check())
+        <li>
+          <a href="#" class="text-white">
+            <span class="icon"><i class="mdi mdi-account"></i></span>
+            <span class="menu-item-label">{{ Auth::user()->name }}</span>
+          </a>
+        </li>
+        <li>
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit" class="text-white hover:text-gray-200 w-full text-left">
+              <span class="icon"><i class="mdi mdi-logout"></i></span>
+              <span class="menu-item-label">Log Out</span>
+            </button>
+          </form>
+        </li>
+      @else
+        <li>
+          <a href="{{ route('login') }}" class="text-white hover:text-gray-200">
+            <span class="icon"><i class="mdi mdi-login"></i></span>
+            <span class="menu-item-label">Log In</span>
+          </a>
+        </li>
+      @endif
+    </ul>
+  </div>
 </aside>
