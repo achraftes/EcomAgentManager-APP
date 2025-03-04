dc8e52fd20b75478c3698c679603a74704cf9303
achraf <achraf04chke@gmail.com>
2025-03-04 14:46:56 +0000

    style: Change text color from white to black for mediaBuyers Page

--- resources/views/mediaBuyers/index.blade.php
+++ resources/views/mediaBuyers/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container mx-auto px-4 sm:px-8">
     <div class="py-8">
         <div class="flex justify-between items-center">
-            <h2 class="text-2xl font-semibold leading-tight text-white">Media Buyers Page</h2>
+            <h2 class="text-2xl font-semibold leading-tight text-black">Media Buyers Page</h2>
             <div class="flex space-x-2 items-center">
                 <a class="btn btn-secondary bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-600 transition duration-300" href="{{ route('mediaBuyers.create') }}">Add Media Buyer</a>
                 <a class="btn btn-primary bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-600 transition duration-300" href="#" id="exportLeadBtn">Export Media Buyers File</a>
