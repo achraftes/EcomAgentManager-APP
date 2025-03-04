383bb2558f6b3c4fff10833d5642c6dda5532101
achraf <achraf04chke@gmail.com>
2025-03-04 19:11:31 +0000

    Adjust grid layout to add left margin for better spacing from the sidebar.

--- resources/views/test/home.blade.php
+++ resources/views/test/home.blade.php
@@ -15,11 +15,11 @@
                 <div class="bg-yellow-100 p-8 rounded-lg shadow-md text-center text-yellow-800 w-full max-w-2xl"> <!-- Ajout de w-full et max-w-2xl pour la largeur, et p-8 pour le padding -->
                     <h2 class="text-2xl font-semibold mb-4">Oops! No Appointments Today 😟</h2>
                     <p class="text-lg mb-6">There are no leads with "RDV le" status scheduled for today.</p>
-                    <a href="{{ route('leads.create') }}" class="mt-4 inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300">Create New Lead</a>
                 </div>
             </div>
         @else
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 ml-8">
+
                 @foreach($leads as $lead)
                     @if(\Carbon\Carbon::parse($lead->comment)->format('Y-m-d') == $today)
                         <a href="{{ route('leads.show', $lead->id) }}" class="block bg-white p-6 rounded-lg shadow-md hover:bg-gray-50 transition duration-300">
