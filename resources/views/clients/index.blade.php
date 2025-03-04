d9e21eeab55e601ca5eacf72ae87b7198896ebb2
achraf <achraf04chke@gmail.com>
2025-03-04 21:37:45 +0000

    Changed the icon on the client page

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -40,14 +40,18 @@
                                                 <div class="buttons right nowrap">
                                                     {{-- Voir les détails --}}
                                                     <a href="{{ route('clients.show', $client->id) }}" class="button small blue">
-                                                        <span class="icon"><i class="mdi mdi-eye"></i></span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM2.458 12C3.732 7.943 7.612 5 12 5c4.388 0 8.268 2.943 9.542 7-.274 1.07-1.272 2.072-2.317 2.318A8.986 8.986 0 0 1 12 19a8.986 8.986 0 0 1-7.225-3.682A6.997 6.997 0 0 1 2.458 12z" />
+                                                    </svg>
                                                     </a>
                                                     {{-- Supprimer --}}
                                                     <form action="{{ route('clients.destroy', $client->id) }}" method="POST" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer ce client?');" class="inline-block">
                                                         @csrf
                                                         @method('DELETE')
                                                         <button type="submit" class="button small red">
-                                                            <span class="icon"><i class="mdi mdi-trash-can"></i></span>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                  </svg>
                                                         </button>
                                                     </form>
                                                 </div>
